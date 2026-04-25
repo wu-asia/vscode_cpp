a6bac1ac20967179df41fd359cdef9df8b8b8e87
AsiaWu_a <wuasia2007@hotmail.com>
2026-04-24 23:59:29 -0700

    23:59F

--- this/heap.cpp
+++ this/heap.cpp
@@ -1,65 +1,23 @@
-// #include<iostream>
-// using namespace std;
-
-// const int N = 1e6 + 10;
-// //堆的实现
-
-// int n;
-// int heap[N];
-// //向上调整算法
-// void up(int child)
-// {
-//     int parent = child / 2;
-//     while(parent >= 1 && heap[child] > heap[parent])
-//     {
-//         swap(heap[child], heap[parent]);
-//         child = parent;
-//         parent = child / 2;
-//     }
-// }
-
-// void down(int parent)
-// {
-//     int child = parent * 2;
-//     while(child <= n)
-//     {
-//         //找出最大的孩子
-//         if(child + 1 <= n && heap[child + 1] > heap[child])
-//             child++;
-//         if(heap[parent] >= heap[child]) return;
-
-//         swap(heap[parent], heap[child]);
-//         parent = child;
-//         child = parent * 2;
-//     }
-// }
-// int main()
-// {
-
-//     return 0;
-// }
-
-
 #include<iostream>
+#include<algorithm>
+
 using namespace std;
 
 const int N = 1e6 + 10;
 int heap[N];
 int n;
 
-//向上调整算法(大根堆)
 void up(int child)
 {
     int parent = child / 2;
     while(parent >= 1 && heap[parent] < heap[child])
     {
-        swap(heap[parent], heap[child]);
+        swap(heap[child], heap[parent]);
         child = parent;
         parent = child / 2;
     }
 }
 
-//向下调整算法(大根堆)
 void down(int parent)
 {
     int child = parent * 2;
@@ -67,7 +25,7 @@ void down(int parent)
     {
         if(child + 1 <= n && heap[child + 1] > heap[child])
             child++;
-        if(heap[parent] > heap[child]) 
+        if(heap[parent] > heap[child])
             return;
         swap(heap[parent], heap[child]);
         parent = child;
@@ -75,13 +33,12 @@ void down(int parent)
     }
 }
 
-//插入元素
 void push(int ele)
 {
     heap[++n] = ele;
     up(n);
 }
-//删除元素
+
 void pop()
 {
     swap(heap[1], heap[n]);
@@ -89,13 +46,11 @@ void pop()
     down(1);
 }
 
-//返回堆顶元素
 int top()
 {
     return heap[1];
 }
 
-//堆的大小
 int size()
 {
     return n;
